381282087a8d30f70927ee734975f4b5d6d3ac82
mohamedelmofy <77664985+mohamedelmofy@users.noreply.github.com>
2022-01-27 20:12:40 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+print('<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<ALFROON>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'
+      )
+print('<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<ALFROON>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'
+      )
+
+print('<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<ALFROON>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'
+      )
+print('<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<ALFROON>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'
+      )
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+
+    print("HELLO")
+    print("welcom to ALFROON ")
+    import socket
+
+# import sys
+
+    print('Enter your DNS or Target: ')
+    hostname = input()
+    ip = socket.gethostbyname(hostname)
+    print('Host Name Is: ', hostname, '\n' 'Target Ip Is: ', ip)
+print('made py ALFROON'
+      )
